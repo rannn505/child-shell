c10b0f72e14fa8d59a1424818ae8f899e008552c
Ran Cohen <rannn505@outlook.com>
2017-02-26 04:19:32 +0200

    3.1.0.3

--- test/shell.spec.js
+++ test/shell.spec.js
@@ -8,7 +8,7 @@ const expect = chai.expect;
 chai.use(chaiAsPromised);
 
 // test files
-const shell = require(path.join(__dirname, '..', 'dist', 'index'));
+const shell = require(path.join(__dirname, '..', 'dist', 'shell')).Shell;
 
 
 describe('Shell', () => {
