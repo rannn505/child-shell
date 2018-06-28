8b3bab5e898e18cad5bb5a04299c40dde29713e4
Hamish Blake <hamish.blake@arup.com>
2018-06-28 12:34:15 +1000

    add string escaping

--- src/Utils.js
+++ src/Utils.js
@@ -34,27 +34,21 @@ export const ShellWrite = (stream, data) => {
 export const toPS = (value) => {
   switch (Object.prototype.toString.call(value).slice(8, -1)) {
     case 'String':
+      if (value) value = value.replace(/[\'\"]/g, '`$&');
       return /\s/.test(value) || (value.indexOf('<') !== -1 && value.indexOf('>') !== -1) ? '"'+value+'"' : value;
-      break;
     case 'Number':
       return value;
-      break;
     case 'Array':
       return value;
-      break;
     case 'Object':
       return `@${JSON.stringify(value).replace(/:/g, '=').replace(/,/g, ';')}`;
-      break;
     case 'Boolean':
       return value ? '$True' : '$False';
-      break;
     case 'Date':
       return value.toLocaleString();
-      break;
     case 'Undefined' || 'Null':
       // param is switch
       return value;
-      break;
     default:
       return /\s/.test(value) ? '"'+value+'"' : value;
   }
