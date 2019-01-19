d75f4bc4a81232dbd25a52e00e3e412cd0ccb7a7
Ran Cohen <rannn505@outlook.com>
2019-01-19 16:31:27 +0200

    4-rc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
       "lines-between-class-members": 0,
       "arrow-parens": 0,
       "no-unused-expressions": 0,
-      
+      "no-param-reassign": 0,
+      "no-console": "off",
     }
 };
