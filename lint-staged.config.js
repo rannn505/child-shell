91f2c6fd855faaa540adcaae042b07bf248284c8
Ran Cohen <rannn505@outlook.com>
2021-02-21 23:39:27 +0200

    husky5 + lint-staged.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   '**/*.{js,ts}?(x)': (filenames) => [
     'npm run types:check',
-    `eslint --cache --ext js,ts --ignore-path .gitignore ${filenames.join(' ')}`,
     `prettier --check --ignore-path .gitignore ${filenames.join(' ')}`,
+    `eslint --cache --ext js,ts --ignore-path .gitignore ${filenames.join(' ')}`,
   ],
 };
