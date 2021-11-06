8213ae4fdb87601af0be1bc0c4ec40baed2bb159
Ran Cohen <rannn505@outlook.com>
2021-11-07 00:52:16 +0200

    versions

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,5 @@ module.exports = {
   '**/*.{js,ts}?(x)': (filenames) => [
     `eslint --cache --ignore-path .gitignore ${filenames.join(' ')}`,
     `prettier --check --ignore-path .gitignore ${filenames.join(' ')}`,
-    'tsc --build --clean tsconfig.build.json',
   ],
 };
